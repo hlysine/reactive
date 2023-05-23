602ed1999817f6a0ec0cdfc6af677d6aba4af6b9
Henry Lin <henry.ys.lin@gmail.com>
2023-05-23 15:28:54 +0800

    test: add test for reactive re-render

--- src/__tests__/component.test.tsx
+++ src/__tests__/component.test.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { makeReactive } from '..';
-
-const TestComponent = makeReactive(function TestComponent() {
-  return <p>Test component</p>;
-});
+import { makeReactive, ref } from '..';
+import { perf, wait } from 'react-performance-testing';
+import 'jest-performance-testing';
 
 describe('Reactive component', () => {
   it('renders without crashing', async () => {
-    const { findByText } = render(<TestComponent />);
+    const Tester = makeReactive(function Tester() {
+      return <p>Test component</p>;
+    });
+
+    const { findByText } = render(<Tester />);
     const content = await findByText('Test component');
     expect(content).toBeTruthy();
+  });
+  it('react to ref changes', async () => {
+    const count = ref(0);
+    const Tester = makeReactive(function Tester() {
+      return <p>{count.value}</p>;
+    });
+
+    const { renderCount } = perf(React);
+
+    const { findByText } = render(<Tester />);
+
+    // count should first be 0
+    const content = await findByText('0');
+    expect(content).toBeTruthy();
+
+    act(() => {
+      count.value++;
+    });
+
+    await wait(async () => {
+      // should re-render with updated value
+      expect(renderCount.current.Tester).toBeRenderedTimes(2);
+      const content = await findByText('1');
+      expect(content).toBeTruthy();
+    });
   });
 });

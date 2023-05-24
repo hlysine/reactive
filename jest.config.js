5056c2d001f392dea1fd5486a417f7cada3f40cc
Henry Lin <henry.ys.lin@gmail.com>
2023-05-24 15:00:59 +0800

    test: move test setup to inside test folder

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/setupTests.ts'],
-  testPathIgnorePatterns: ['example', 'node_modules'],
+  setupFiles: ['<rootDir>/src/__tests__/setup.ts'],
+  testPathIgnorePatterns: ['example', 'node_modules', '__tests__/setup.ts'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',

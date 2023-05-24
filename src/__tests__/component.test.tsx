5e0ffc556efa9692309e5489bca8d6c5c1507209
Henry Lin <henry.ys.lin@gmail.com>
2023-05-24 13:33:19 +0800

    test: add more tests for makeReactive

--- src/__tests__/component.test.tsx
+++ src/__tests__/component.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { act, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { makeReactive, ref, useWatchEffect } from '..';
+import { makeReactive, reactive, ref, useWatchEffect } from '..';
 import { perf, wait } from 'react-performance-testing';
 import 'jest-performance-testing';
 
-describe('Reactive component', () => {
+describe('makeReactive', () => {
   it('renders without crashing', async () => {
     const Tester = makeReactive(function Tester() {
       return <p>Test component</p>;
@@ -36,6 +36,90 @@ describe('Reactive component', () => {
     await wait(async () => {
       // should re-render with updated value
       expect(renderCount.current.Tester).toBeRenderedTimes(2);
+      const content = await findByText('1');
+      expect(content).toBeTruthy();
+    });
+  });
+  it('batches state updates', async () => {
+    const obj = reactive({ a: 1, b: 2 });
+    const Tester = makeReactive(function Tester() {
+      return (
+        <p data-testid="obj">
+          a: {obj.a}, b: {obj.b}
+        </p>
+      );
+    });
+
+    const { renderCount } = perf(React);
+
+    const { findByTestId } = render(<Tester />);
+
+    const content = await findByTestId('obj');
+    expect(content.innerHTML).toContain('a: 1, b: 2');
+
+    act(() => {
+      obj.a++;
+      obj.b++;
+    });
+
+    await wait(async () => {
+      // should re-render with updated value
+      expect(renderCount.current.Tester).toBeRenderedTimes(2);
+      const content = await findByTestId('obj');
+      expect(content.innerHTML).toContain('a: 2, b: 3');
+    });
+  });
+  it('does not re-render when unrelated state changes', async () => {
+    const obj = reactive({ a: 1, b: 2 });
+    const Tester = makeReactive(function Tester() {
+      return <p>{obj.a}</p>;
+    });
+
+    const { renderCount } = perf(React);
+
+    const { findByText } = render(<Tester />);
+
+    const content = await findByText('1');
+    expect(content).toBeTruthy();
+
+    act(() => {
+      obj.b++;
+    });
+
+    await wait(async () => {
+      // should not re-render
+      expect(renderCount.current.Tester).toBeRenderedTimes(1);
+      const content = await findByText('1');
+      expect(content).toBeTruthy();
+    });
+  });
+  it('does not re-render when state changes in nested watcher', async () => {
+    const obj = reactive({ a: 1, b: 2 });
+    const mockEffect = jest.fn();
+
+    const Tester = makeReactive(function Tester() {
+      useWatchEffect(() => {
+        mockEffect(obj.b);
+      });
+      return <p>{obj.a}</p>;
+    });
+
+    const { renderCount } = perf(React);
+
+    const { findByText } = render(<Tester />);
+
+    expect(mockEffect.mock.calls).toHaveLength(1);
+    const content = await findByText('1');
+    expect(content).toBeTruthy();
+
+    act(() => {
+      obj.b++;
+    });
+
+    await wait(async () => {
+      // should not re-render, but should trigger effect again
+      expect(renderCount.current.Tester).toBeRenderedTimes(1);
+      expect(mockEffect.mock.calls).toHaveLength(2);
       const content = await findByText('1');
       expect(content).toBeTruthy();
     });
